4a4413f6f59dc165c9a125f5cbb628abf2febc21
IvanSSantana <ivansantospriv@gmail.com>
2025-08-28 12:25:35 -0300

    Estilizando footer e imagem principal da faixa de áudio.

--- musicApp/App.js
+++ musicApp/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View  } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image  } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import Slider from '@react-native-community/slider';
 
 const { width, height } = Dimensions.get('window');
 
@@ -9,7 +10,16 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.main}>
+        <View style={[styles.imageWrapper, styles.elevation]}>
+          <Image
+            source={require('./assets/img/silvinha.png')}
+            style={styles.musicImage}
+            />
+        </View>
+      </View>
 
+      <View>
+        <Slider/>
       </View>
 
       <View style={styles.footer}>
@@ -41,7 +51,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#222831',
+  },
+  main: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  footer: {
+    width: width,
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderTopColor: '#393E45',
+    borderTopWidth: 1,
+  },
+  iconWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%'
+  },
+  imageWrapper: {
+    width: 340,
+    height: 360,
+    marginVertical: 20,
+  },
+  elevation:{
+    elevation: 5,
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84
+  },
+  musicImage:{
+    width:'100%',
+    height: '100%',
+    borderRadius: 15
   },
 });
